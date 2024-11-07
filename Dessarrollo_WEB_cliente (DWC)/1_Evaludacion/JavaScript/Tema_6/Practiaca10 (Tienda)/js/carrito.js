751ff17c5350a01a78daefbbf6fcfee578cb38b1
Shengye <shengye472@gmail.com>
2024-11-07 13:39:51 +0100

    work Shop 2024/11/07

--- Dessarrollo_WEB_cliente (DWC)/1_Evaludacion/JavaScript/Tema_6/Practiaca10 (Tienda)/js/carrito.js
+++ Dessarrollo_WEB_cliente (DWC)/1_Evaludacion/JavaScript/Tema_6/Practiaca10 (Tienda)/js/carrito.js
@@ -1,6 +1,6 @@
 class Carrito {
 	constructor(id) {
-		
+		this.id = id;
 	}
 
 	anyadeArticulo(articulo) {
@@ -15,11 +15,84 @@ class Carrito {
 	}
 
 	borraArticulo(codigo) {
+		lista.splice(lista.findIndex(a => a.codigo === codigo),1)
+		if (lista.length === 0) {
+			document.getElementById("miDialogo").close()
+		}
 	}
 
 	modificaUnidades(codigo, n) {
+		let producto = lista.find(a => a.codigo === codigo)
+		producto.unidades += n
+		if (producto.unidades === 0) {
+			this.borraArticulo(codigo)
+		}
+		producto.total = producto.precio * producto.unidades
 	}
 
 	verCarrito() {
+		document.getElementById("miDialogo").showModal()
+		const dialogo = document.getElementById("dialogContent")
+		dialogo.innerHTML = ""
+		let total = 0
+		let contenidoTabla = `
+			<table class="table table-striped table-bordered">
+				<theader>
+					<tr class="table-light">
+						<th>&nbsp</th>
+						<th>nombre</th>
+						<th>descripcion</th>
+						<th>precio</th>
+						<th>unidades</th>
+						<th>total</th>
+						<th>&nbsp</th>
+					</tr>
+				</theader>
+				<tbody>
+	`
+		lista.forEach(a => {
+			let tabla = `
+			<tr>
+				<td><img width = 60px src="assets/${a.codigo}.jpg" ></td>
+				<td class="align-middle">${a.nombre}</td>
+				<td class="align-middle">${a.descripcion}</td>
+				<td class="align-middle">${a.precio}</td>
+				<td class="align-middle">${a.unidades}</td>
+				<td class="align-middle">${a.total}</td>
+				<td class="align-middle">
+					<button class="btn btn-primary" id="${a.codigo}">+</button>
+						<button class="btn btn-warning" id="${a.codigo}">-</button>
+						<button class="btn btn-danger" id="${a.codigo}">Borrar</button>
+				</td>
+			</tr>`
+			total += a.total
+			contenidoTabla += tabla
+		})
+
+
+
+		contenidoTabla += `
+				</tbody>
+			</table>`
+
+		dialogo.innerHTML = contenidoTabla
+
+		document.getElementById("total").innerHTML = total
+		
+		Array.from(dialogo.getElementsByClassName("btn-danger")).forEach(a => {
+			a.addEventListener("click",() => {
+				this.borraArticulo(a.id)
+				verCarro()
+			})})
+		Array.from(dialogo.getElementsByClassName("btn-primary")).forEach(a => {
+			a.addEventListener("click",() => {
+				this.modificaUnidades(a.id,1)
+				verCarro()
+			})})
+		Array.from(dialogo.getElementsByClassName("btn-warning")).forEach(a => {
+			a.addEventListener("click",() => {
+				this.modificaUnidades(a.id,-1)
+				verCarro()
+			})})
 	}
 }
--- Dessarrollo_WEB_cliente (DWC)/1_Evaludacion/JavaScript/Tema_6/Practiaca10 (Tienda)/js/tienda.js
+++ Dessarrollo_WEB_cliente (DWC)/1_Evaludacion/JavaScript/Tema_6/Practiaca10 (Tienda)/js/tienda.js
@@ -1,6 +1,6 @@
 criterios = ["Sin ordenar", "Ascendente por precio", "Descendente por precio"]
 
-const carrito = new Carrito
+const carrito = new Carrito(1)
 
 
 function creaListaCriterios() {
@@ -46,53 +46,17 @@ function buscarArticulo(id) {
 const lista = []
 
 function ponArticuloEnCarrito(articulo) {
-	carrito.anyadeArticulo(articulo)	
+	carrito.anyadeArticulo(articulo)
 	console.log(lista)
-	verCarro()
 }
 
 
 function verCarro() {
-	const dialogo = document.getElementById("dialogContent")
-	dialogo.innerHTML = ""
-	let contenidoTabla = `
-	<table class="table table-striped table-bordered">
-		<theader>
-			<tr class="table-light">
-				<th>&nbsp</th>
-				<th>nombre</th>
-				<th>descripcion</th>
-				<th>precio</th>
-				<th>unidades</th>
-				<th>total</th>
-				<th>&nbsp</th>
-			</tr>
-		</theader>
-		<tbody>
-	`
-	lista.forEach(a => {
-		let tabla = `
-			<tr>
-				<td><img width = 60px src="assets/${a.codigo}.jpg" ></td>
-				<td class="align-middle">${a.nombre}</td>
-				<td class="align-middle">${a.descripcion}</td>
-				<td class="align-middle">${a.precio}</td>
-				<td class="align-middle">${a.unidades}</td>
-				<td class="align-middle">${a.total}</td>
-				<td class="align-middle">
-					<button class="btn btn-primary" id="inc-${a.codigo}">+</button>
-						<button class="btn btn-warning" id="dec-${a.codigo}">-</button>
-						<button class="btn btn-danger" id="del-${a.codigo}">Borrar</button>
-				</td>
-		</tr>`
-		contenidoTabla += tabla
-	})
-
-	contenidoTabla += `
-	</tbody>
-	</table>`
-
-	dialogo.innerHTML = contenidoTabla
+	if (lista.length === 0) {
+		alert("El Carro esta vacio")
+	} else {
+		carrito.verCarrito()
+	}
 }
 
 function efectuaPedido() {
@@ -102,8 +66,7 @@ function efectuaPedido() {
 window.onload = () => {
 	pintaArticulos()
 	ponIdBotonArticulo()
-	verCarro()
-	document.getElementById("miCarro").onclick = () => document.getElementById("miDialogo").showModal()
+	document.getElementById("miCarro").onclick = () => verCarro()
 	document.getElementById("btnCierraDialog").onclick = () => document.getElementById("miDialogo").close()
 }
 
