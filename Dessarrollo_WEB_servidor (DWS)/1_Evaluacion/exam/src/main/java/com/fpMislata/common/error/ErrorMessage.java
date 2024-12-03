64dfc0bf6e034c34b7c3d7f22f277d6d95613c1e
Shengye <shengye472@gmail.com>
2024-12-03 10:08:40 +0100

    DWS: prepare for exam

---
+++ Dessarrollo_WEB_servidor (DWS)/1_Evaluacion/exam/src/main/java/com/fpMislata/common/error/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.fpMislata.common.error;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ErrorMessage {
+    private final String error;
+    private final String message;
+
+    public ErrorMessage(Exception e) {
+        this.error = e.getClass().getSimpleName();
+        this.message = e.getMessage();
+    }
+}
---
+++ Dessarrollo_WEB_servidor (DWS)/1_Evaluacion/exam/src/test/java/com/fpMislata/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpMislata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
